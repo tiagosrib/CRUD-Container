172cbe80a1161fd90ea0b5003786ce149c2abba7
tiagosrib <108826613+tiagosrib@users.noreply.github.com>
2022-11-22 13:56:31 -0300

    Create movimentacoes.php

---
+++ container/movimentacoes.php
@@ -0,0 +1,91 @@
+<?php
+
+include('conexao.php');
+
+$sql_movimentacao = "SELECT * FROM movimentacao ORDER BY fim";
+$query_movimentacao = $mysqli->query($sql_movimentacao) or die($mysqli->error);
+
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css" integrity="sha384-MCw98/SFnGE8fJT3GXwEOngsV7Zt27NXFoaoApmYm81iuXoPkFOJwJ8ERdknLPMO" crossorigin="anonymous">
+    <link href="//cdn.datatables.net/1.10.15/css/jquery.dataTables.min.css" rel="stylesheet">
+
+
+    <title>Movimentações Cadastrados</title>
+</head>
+<body>
+    <nav>
+        <ul class="nav justify-content-center">
+            <li class="nav-item">
+                <a class="nav-link active" href="cadastro_containers.php">Cadastro</a>
+            </li>
+            <li class="nav-item">
+                <a class="nav-link" href="containers.php">Containers</a>
+            </li>
+            <li class="nav-item">
+                <a class="nav-link" href="movimentacoes.php">Movimentações</a>
+            </li>
+            <li class="nav-item">
+                <a class="nav-link" href="relatorioie.php">Relatório I/E</a>
+            </li>
+            <li class="nav-item">
+                <a class="nav-link" href="relatorio_mov.php">Relatório Movimentações</a>
+            </li>
+
+        </ul>
+    </nav>
+
+    <h1 style="text-align:center;">Movimentações</h1>
+    <div style="padding:0 5% ;">
+        <table id="tabela1" class="table table-striped">
+            <thead>
+                <tr>
+                    <th scope="col">Container</th>
+                    <th scope="col">Movimentação</th>
+                    <th scope="col">Início</th>
+                    <th scope="col">Fim</th>
+                    <th scope="col">Ações</th>
+                </tr>
+            </thead>
+            <tbody>
+                <?php while ($movimentacao = $query_movimentacao->fetch_assoc()) {
+                    ?>
+                <tr>
+                    <td><?php echo $movimentacao['container'] ?></td>
+                    <td><?php echo $movimentacao['movimentacao'] ?></td>
+                    <td><?php echo $movimentacao['inicio'] ?></td>
+                    <td><?php echo $movimentacao['fim'] ?></td>
+                    <td>
+                        <a href="editar_movimentacao.php?id=<?php echo $movimentacao['id'] ?>"><button type="submit" class="btn btn-primary">Editar</button></a>
+                        <a href="excluir_movimentacao.php?id=<?php echo $movimentacao['id'] ?>"><button type="submit" class="btn btn-primary">Excluir</button></a>
+                    </td>
+                <?php } 
+                ?>
+                </tr>
+            </tbody>
+        </table>
+    </div>    
+
+    <script src="//code.jquery.com/jquery-3.2.1.min.js"></script>
+    <script src="//cdn.datatables.net/1.10.15/js/jquery.dataTables.min.js"></script>
+    <script>
+        $(document).ready(function(){
+            $('#tabela1').DataTable({
+                    "language": {
+                        "lengthMenu": "Mostrando _MENU_ registros por página",
+                        "zeroRecords": "Nenhum registro encontrado",
+                        "info": "Mostrando página _PAGE_ de _PAGES_",
+                        "infoEmpty": "Nenhum registro disponível",
+                        "infoFiltered": "(filtrado de _MAX_ registros no total)"
+                    }
+                });
+        });
+    </script>
+
+</body>
+</html>
