34189a1d7d2de59cbdab8f418db29ff43a000ffe
tiagosrib <108826613+tiagosrib@users.noreply.github.com>
2022-12-06 19:50:56 -0300

    Create excluir_usuarios.php

---
+++ container/excluir_usuarios.php
@@ -0,0 +1,42 @@
+<?php
+
+include('lib/session.php');
+include('lib/conexao.php');
+
+if($_SESSION['tipo'] !=1 ){
+    die('O acesso a essa página é restrito! <a href="relatorio_mov.php"> VOLTAR</a>');    
+}
+
+if(isset($_POST['excluir'])) {
+    $id = intval($_GET['id']);
+
+    $sql_code = " DELETE FROM senhas WHERE id = '$id'";
+    $sql_query = $mysqli->query($sql_code) or die($mysqli->error);
+
+    if($sql_query) {
+        ?> 
+        <h1>Excluido com Sucesso !! </h1>
+        <p>Voltar para <a href="listar_usuarios.php">Lista de Usuários</a></p>
+        <?php
+        die();
+    }
+}
+
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Exlcuir Acesso</title>
+</head>
+<body>
+    <h1>Tem Certeza que deseja excluir o Acesso do Usuário ??</h1>
+    <form action="" method="post">
+        <a style="margin:0 50px" href="containers.php">Não</a>
+        <button type="submit" name="excluir" value="1" class="btn btn-primary">Excluir</button>
+    </form>
+    
+</body>
+</html>
